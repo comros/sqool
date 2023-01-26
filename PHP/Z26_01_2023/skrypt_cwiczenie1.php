14f6b6dc092718ab3b2646bd7c0122cd7cf7f897
comros <40005394+comros@users.noreply.github.com>
2023-01-26 12:09:04 +0100

    Create skrypt_cwiczenie1.php

---
+++ PHP/Z26_01_2023/skrypt_cwiczenie1.php
@@ -0,0 +1,39 @@
+<?php
+echo "Dziękujemy! Rejestracja przebiegła pomyślnie.</br></br>";
+echo "Wprowadzone dane:</br>";
+echo "Nazwisko: ".$_POST['nazw']."<br />";
+echo "Imię: ".$_POST['imie']."<br />";
+echo "Zawód: ".$_POST['zaw']."<br />";
+echo "Adres e-mail: ".$_POST['adr']."<br />";
+
+if(!isSet($_POST['wykszt']))
+echo "Proszę zaznaczyć pole Wykształcenie.";
+else
+echo "Wykształcenie: ".$_POST['wykszt']."</br></br>";
+echo "Dodatkowe uprawnienia: ";
+if(!empty($_POST['uprawnienia'])) {
+	echo "<ul>";
+	foreach ($_POST['uprawnienia'] as $wartosc) {
+		echo "<li>$wartosc</li>";
+	}
+	echo "</ul>";
+}
+else {
+	echo "<p>".$_POST['nazw']." nie zna żadnego języka.<p>";
+}
+echo "<br />";
+
+if(empty($_POST['opcje'])) 
+echo "Nie zgadza się na przetwarzanie danych osobowych.";	
+else 
+echo "Zgadza się na przetwarzanie danych osobowych.";
+echo "<br /><br />";
+
+if(is_uploaded_file($_FILES['plik1']['tmp_name']))
+{
+	echo "Informacje na temat załącznika.<br>";
+	echo "Został odebrany plik o nazwie: <b>".$_FILES['plik1']['name']."</b><br>";
+	echo "Typ pliku: <b>".$_FILES['plik1']['type']."</b><br>";
+}
+
+?>
