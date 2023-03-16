8ad146c42251508843441c530394d5aeb97eb420
comros <40005394+comros@users.noreply.github.com>
2023-03-16 12:11:33 +0100

    Update 16_03_23.php

--- PHP/MySQL_i_PHP/16_03_23.php
+++ PHP/MySQL_i_PHP/16_03_23.php
@@ -11,6 +11,24 @@ if($do_bazy)
     {
         echo $tab1[0].' '.$tab1[1].' '.$tab1[2].'<br>';
     }
+
+    // cw1. Wyswietl adresy wszystkich klientów z tabeli klient
+    $zapyt2 = 'SELECT Nazwisko, Imie, Kod_pocztowy, Miejscowosc, Ulica, Nr_Domu 
+    FROM klient WHERE Telefon != 0';
+    $wynik2 = mysqli_query($do_bazy, $zapyt2);
+
+    echo '<br><h3>Testowanie mysqli_fetch_array()</h3>';
+
+    while($tab2 = mysqli_fetch_array($wynik2))
+    {
+        echo '<b>'.$tab2['Nazwisko']
+        .' '.$tab2['Imie']
+        .'</b> Adres zamieszkania: <b>'.$tab2['Miejscowosc']
+        .', ul.'.$tab2['Ulica'].' '.$tab2['Nr_Domu'].'</b><br>';
+    }
+
+    echo 'W tabeli klient zapisano informacje o '.mysqli_num_rows($wynik2).' klientach<br>';
+
 }
 else
 {
