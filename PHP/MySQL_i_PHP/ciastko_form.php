2e9b2e61142ec6ce1f1abc675299afea1501de6a
comros <40005394+comros@users.noreply.github.com>
2023-05-11 12:14:39 +0200

    Update ciastko_form.php

--- PHP/MySQL_i_PHP/ciastko_form.php
+++ PHP/MySQL_i_PHP/ciastko_form.php
@@ -1,5 +1,17 @@
 <?php
 
+// Ustawia licznik odwiedzin
+if(isset($_COOKIE['odwiedziny']))
+{
+    $ilosc_odw = $_COOKIE['odwiedziny']+1;
+}
+else
+{
+    $ilosc_odw = 1;
+}
+setcookie('odwiedziny', $ilosc_odw, time() + 604800);
+
+// Wypisuje tekst powitalny, imie, nazwisko lub usuwa/tworzy ciastko
 if(isset($_COOKIE['dane'])) 
 {
     echo "Witamy po raz kolejny <b>{$_COOKIE['dane']}</b><br>";
@@ -7,13 +19,17 @@ if(isset($_COOKIE['dane']))
     if(isset($_POST['delete_cookie']))
     {
         setcookie('dane', "{$_POST['imie']} {$_POST['nazw']}", time() - 604800);
-        echo "Usunięto ciastko.";
+        setcookie('odwiedziny', $ilosc_odw, time() - 604800);
+        echo "Usunięto ciastko.<br>";
     }
 }
 else 
 {
+    // Plik cookie jest ważny tydzień
     setcookie('dane', "{$_POST['imie']} {$_POST['nazw']}", time() + 604800);
-    echo "Stworzono ciastko";
+    echo "Stworzono ciastko.<br>";
 }
+
+echo "Ilość odwiedzin: <b>$ilosc_odw</b><br>";
 
 ?>
