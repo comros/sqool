36516d74af40fe3460c6f8a9e6435e9664be1ff6
comros <40005394+comros@users.noreply.github.com>
2023-06-01 11:12:38 +0200

    Create wprowadzenie.php

---
+++ PHP/szyfrowanie/wprowadzenie.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+    sha1(string, binary:bool) : string
+    sha1_file(filename, binary:bool) : string | false
+    crc32(string) : int
+    md5(string, binary:bool) : string
+    hash(algo:stirng,data:string,binary:bool,options:array) : string
+    crypt(string, salt:string) : string
+    password_hash(password:string, algo:string|int|null, options:array) : string
+*/ 
+
+?>
